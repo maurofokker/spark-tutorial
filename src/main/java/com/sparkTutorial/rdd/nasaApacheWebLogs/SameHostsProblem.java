b14e90b1d77fc61675c34ebb20ec760b92275834
maurofokker <mgaldames@gmail.com>
2018-01-21 00:10:10 -0300

    using spark intersection

--- src/main/java/com/sparkTutorial/rdd/nasaApacheWebLogs/SameHostsProblem.java
+++ src/main/java/com/sparkTutorial/rdd/nasaApacheWebLogs/SameHostsProblem.java
@@ -1,5 +1,9 @@
 package com.sparkTutorial.rdd.nasaApacheWebLogs;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class SameHostsProblem {
 
     public static void main(String[] args) throws Exception {
@@ -19,5 +23,24 @@ public class SameHostsProblem {
 
            Make sure the head lines are removed in the resulting RDD.
          */
+        // run with all cores in local cpu
+        SparkConf sparkConf = new SparkConf().setAppName("sameHosts").setMaster("local[2]");
+
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+
+        // load input files into RDD
+        JavaRDD<String> julyFirstLogs = sparkContext.textFile("in/nasa_19950701.tsv");
+        JavaRDD<String> augustFirstLogs = sparkContext.textFile("in/nasa_19950801.tsv");
+
+        // split by tab and get the first column
+        JavaRDD<String> julyFirstHosts = julyFirstLogs.map(line -> line.split("\t")[0]);
+        JavaRDD<String> augustFirstHosts = augustFirstLogs.map(line -> line.split("\t")[0]);
+
+        JavaRDD<String> intersection = julyFirstHosts.intersection(augustFirstHosts);
+
+        // remove header
+        JavaRDD<String> cleanedHostIntersection = intersection.filter(host -> !host.equals("host"));
+
+        cleanedHostIntersection.saveAsTextFile("out/nasa_logs_same_hosts.csv");
     }
 }
